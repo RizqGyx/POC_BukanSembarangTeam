5e3ce11923fc2be6080a90c1587f495ba6c994fa
Berzki <mhdrizky32@gmail.com>
2026-05-19 12:11:58 +0700

    Initial Commit

---
+++ POC_BukanSembarang/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  POC_BukanSembarang
+//
+//  Created by Muhammad Rizki on 19/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ POC_BukanSembarang/POC_BukanSembarangApp.swift
@@ -0,0 +1,17 @@
+//
+//  POC_BukanSembarangApp.swift
+//  POC_BukanSembarang
+//
+//  Created by Muhammad Rizki on 19/05/26.
+//
+
+import SwiftUI
+
+@main
+struct POC_BukanSembarangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
